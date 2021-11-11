d71f9d4bb3e12aac5d4326a581577522c9b6e727
Shodipe Oladele <quickly2kenny@gmail.com>
2021-11-11 13:15:01 +0100

    Initial Commit

---
+++ rock.js
@@ -0,0 +1,51 @@
+/*
+function gameStart(playerSelection, computerSelection)
+if playerSelection === computerSelection
+    gameDrawn
+if playerSelection === rock and computerSelection === scissors
+    player wins
+if playerSelection === rock and computerSelection === papper
+    computer wins
+if playerSelection === scissors and computerSelection === papper
+    playerwins
+if playerSelection === scissors and computer selection === rock
+    computer wins
+if playerSelection == papper and computer selection == scissors
+    computer wins
+if playerSelection == papper and computer selection == rock
+    player wins
+
+
+*/
+function computerPlay() {
+    let computerOption = ['rock', 'papper', 'scissors'];
+    let choice = Math.floor(Math.random() * computerOption.length);
+    return computerOption[choice];
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        let  result = `${playerSelection} against ${computerSelection}. Game Drawn`;
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        result = `You Win. ${playerSelection} beats ${computerSelection}`;
+    } else if (playerSelection === 'rock' && computerSelection === 'papper'){
+        result = `You Lose. ${computerSelection} beats ${playerSelection}`;
+    } else if (playerSelection === 'scissors' && computerSelection === 'papper') {
+        result = `You Win. ${playerSelection} beats ${computerSelection}.`;
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        result = `You Lose. ${computerSelection} beats ${playerSelection}`;
+    } else if (playerSelection === 'papper' && computerSelection === 'scissors') {
+        result = `You Lose. ${computerSelection} beats ${playerSelection}`;
+    } else if (playerSelection === 'papper' && computerSelection === 'rock') {
+        result = `You Win. ${playerSelection} beats ${computerSelection}`;
+    }
+    return result;
+}
+
+
+
+for (i = 0; i < 5; i++) {
+    const playerSelection = prompt("Enter either of 'Rock, Papper or Scissors': ").toLowerCase();
+    const computerSelection = computerPlay();
+    console.log(playRound(playerSelection, computerSelection));
+}
